665333d519d9ed3f7f0c0d4431c6225feb27ca21
SamikshyaBasnet <68534684+SamikshyaBasnet@users.noreply.github.com>
2022-06-27 21:41:53 +0545

    deploying the heroku

---
+++ data.js
@@ -0,0 +1,40 @@
+const data = [{
+        channel: "sammy",
+        description: "Check my dance!",
+        song: "Hi all",
+        likes: " 1003",
+
+        messages: "205 ",
+
+        shares: "105",
+        url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
+
+    },
+
+    {
+        channel: "sammy",
+        description: "Check my dance!",
+        song: "Hi all",
+        likes: " 1003",
+
+        messages: "205 ",
+
+        shares: "105",
+        url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
+
+    },
+    {
+        channel: "sammy",
+        description: "Check my dance!",
+        song: "Hi all",
+        likes: " 1003",
+
+        messages: "205 ",
+
+        shares: "105",
+        url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
+
+    },
+
+]
+export default data;
