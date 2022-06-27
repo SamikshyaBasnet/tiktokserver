cd1f9fb68e071055dd7202302518ead4a77fc57d
SamikshyaBasnet <68534684+SamikshyaBasnet@users.noreply.github.com>
2022-06-27 22:24:23 +0545

    deploying the heroku

--- data.js
+++ data.js
@@ -1,38 +1,34 @@
 const data = [{
         channel: "sammy",
-        description: "Check my dance!",
-        song: "Hi all",
-        likes: " 1003",
-
-        messages: "205 ",
-
-        shares: "105",
-        url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
+        description: "Yayy this works!",
+        song: "King sh*t",
+        likes: "123",
+        messages: "45",
+        shares: "12",
+        url: "https://v16-webapp.tiktok.com/bbd17333f1d3b8067e403db4fbb90dd7/62b9d302/video/tos/useast2a/tos-useast2a-ve-0068c003/8fdec232636d4593bb97b5310556597d/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:3156&bt:1578&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8ZiL98we2Nobfml7Gb&mime_type:video_mp4&qs:0&rc:NGg8NDQzZjc0NDhlZDc3aEBpajt5bmg6ZjR5ZDMzNzczM0A2NjIuMTA0NS4xNmFgNS1eYSNpL2lwcjRvcmhgLS1kMTZzcw%3D%3D&l:202206270955300102450431510A06D9FC"
 
     },
 
     {
-        channel: "sammy",
-        description: "Check my dance!",
-        song: "Hi all",
-        likes: " 1003",
-
+        channel: "vemma",
+        description: "Hi all, Check my dance!",
+        song: "Break it",
+        likes: "1003",
         messages: "205 ",
-
         shares: "105",
         url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
 
     },
     {
-        channel: "sammy",
-        description: "Check my dance!",
-        song: "Hi all",
-        likes: " 1003",
+        channel: "couple",
+        description: "Keep eating lol",
+        song: "Eat Eat",
+        likes: " 2005",
 
-        messages: "205 ",
+        messages: "400 ",
 
-        shares: "105",
-        url: "https://v16-webapp.tiktok.com/c377055c9640ef41f8bbadcc9990a1bc/62ba086a/video/tos/useast2a/tos-useast2a-pve-0037c001-aiso/07766cf422e54694963a6b103e7958e4/?a:1988&ch:0&cr:0&dr:0&lr:tiktok_m&cd:0%7C0%7C1%7C0&cv:1&br:4570&bt:2285&btag:80000&cs:0&ds:3&ft:eXd.6HFJMyq8Zgax8we2NMGQml7Gb&mime_type:video_mp4&qs:0&rc:Zzw2NDRpNzY4NjYzaDs2Z0BpajMzcjM6Zm9rOzMzZjczM0BgMGM1Ly0xXzQxNi4wNmMwYSMyc3JucjRvNi1gLS1kMWNzcw%3D%3D&l:2022062713431601024405522723257425",
+        shares: "100",
+        url: "https://v16-webapp.tiktok.com/5394bb2b187da4f71744c0d7e37e400a/62ba085a/video/tos/alisg/tos-alisg-pve-0037/d356ab5750744c1a8e35ca59ef74c08e/?a=1988&ch=0&cr=0&dr=0&lr=tiktok&cd=0%7C0%7C1%7C0&cv=1&br=2514&bt=1257&btag=80000&cs=0&ds=3&ft=eXd.6HFJMyq8Zp0x8we2NnrTrl7Gb&mime_type=video_mp4&qs=0&rc=PDZnOzo5OTY2Zjc6NGc8PEBpMzo6dmc6ZjxpOjMzODgzNEAuLTRfMC9gNl8xYmFiYS5fYSNzb2pscjQwNWBgLS1kLy1zcw%3D%3D&l=202206271342180102452422261D25C648",
 
     },
 
